e443d4da158ca3e305336d31509115bc60a3da70
Stefano-BML <stefanoml1128@gmail.com>
2023-03-29 21:21:54 -0700

    _printf.c

---
+++ _printf.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+int _printf(const char *format, ...)
+{
+	va_list p;
+	unsigned int i, j;
+
+	types typelist[] = { 
+		{'c', charsp},
+		{'s', strsp},
+		{'\0', NULL}
+	};
+	va_start(p,format);
+
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			for (j = 0; typelist[j].type != '\0'; j++)
+			{
+				if (format[i + 1] == typelist[j].type)
+				{
+					(*typelist[j].f)(&p);
+					i++;
+					break;
+				}
+			}
+				
+		}
+		else
+		{
+			_putchar(format[i]);
+		}
+	}
+	va_end(p);
+	return (0);	
+}

2c718a928d7b25a47bce0e2b600fbb4dcaeae720
Stefano-BML <stefanoml1128@gmail.com>
2023-03-29 21:20:30 -0700

    main.h

--- main.h
+++ main.h
@@ -1,1 +1,19 @@
-#
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct sp {
+
+	char type;
+	void (*f)(va_list *);
+} types;
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+void charsp(va_list *p);
+void strsp(va_list *p);
+#endif
